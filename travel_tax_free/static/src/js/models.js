3a3cb0077099f25bc1ea1a9da6fe0425ef9c266e
Javier Sánchez <jsanchez@traveltaxfree.com>
2026-02-23 16:13:36 +0100

    [FIX] Arreglos en Odoo v16

--- travel_tax_free/static/src/js/models.js
+++ travel_tax_free/static/src/js/models.js
@@ -1,208 +1,85 @@
-odoo.define('pos_taxfree.models2', function(require) {
-    'use strict;'
+odoo.define('pos_taxfree.models_final', function(require) {
+    'use strict';
 
-    var { PosGlobalState, Order } = require('point_of_sale.models');
+    const { PosGlobalState, Order } = require('point_of_sale.models');
     const rpc = require('web.rpc');
     const Registries = require('point_of_sale.Registries');
+    const { Gui } = require('point_of_sale.Gui');
 
-    var old_prototype_posmodel = PosGlobalState.prototype;
-    const PosExtension = (PosGlobalState) =>
-        class PosExtension extends PosGlobalState {
-            setup() {
-                super.setup();
+    const PosTaxFreeGlobal = (PosGlobalState) => class PosTaxFreeGlobal extends PosGlobalState {
+        
+        async push_single_order(order, opts) {
+            if (!order || !order.is_to_taxfree()) {
+                return super.push_single_order(order, opts);
             }
 
-            constructor(obj){
-                super(obj);
-            }
-
-            get_client() {
-                var order = this.get_order();
-                if (order) {
-                    return order.get_partner();
+            try {
+                const client = order.get_partner();
+                if (!client) {
+                    Gui.showPopup('ErrorPopup', { title: 'Error', body: 'Debe seleccionar un cliente' });
+                    return false;
                 }
-                return null;
-            }
 
-            test_tourist() {
-                var client = this.get_client();
-                return new Promise(function (resolve, reject) {
-                    rpc.query({
-                            model: 'res.partner',
-                            method: 'test_tourist_id',
-                            args: [{}, client.id],
-                        }, {
-                        timeout: 3000,
-                        shadow: true,
-                    }).then(function (data) {
-                        if ("code" in data && data["code"] == '0000') {
-                            resolve(data);
-                        } else{
-                            reject(data);
-                        }
-                    }).catch(function () {
-                        reject({
-                            code: '9999',
-                            message: 'error_tourist',
-                        });
-                    })
+                const res_tourist = await rpc.query({
+                    model: 'res.partner',
+                    method: 'test_tourist_id',
+                    args: [client.id],
                 });
-            }
 
-            create_taxfree(name) {
-                var peticion = $.Deferred();
-                var res = rpc.query({
-                            model: 'pos.order',
-                            method: 'generate_taxfree_from_order_name',
-                            args: [{}, name],
-                }, {
-                    shadow: false,
-                }).then(function (data) {
-                    peticion.resolve(data);
-                }).catch(function () {
-                    peticion.reject({
-                        code: '9899',
-                        msg: 'Error creando el tax free',
+                if (res_tourist && res_tourist.code !== '0000') {
+                    Gui.showPopup('ErrorPopup', {
+                        title: 'Validación Tax Free',
+                        body: res_tourist.msg || 'Datos de cliente no válidos',
                     });
-                });
-
-                return peticion;
-
+                    return false;
+                }
+            } catch (err) {
+                console.error("Error validando turista", err);
+                return false;
             }
 
-            get_invoice_number(order) {
-                var peticion = $.Deferred();
-                var res = rpc.query({
+            const server_ids = await super.push_single_order(order, opts);
+
+            if (server_ids && server_ids.length > 0) {
+                try {
+                    const res = await rpc.query({
                         model: 'pos.order',
-                        method: 'search_read',
-                        domain: [['pos_reference', '=', order['name']]],
-                        fields: ['account_move']
-                }).then(function (orders) {
-                    if (orders.length > 0 && orders[0]['account_move'] && orders[0]['account_move'][1]) {
-                        var invoice_number = orders[0]['account_move'][1].split(" ")[0];
-                        order['invoice_number'] = invoice_number;
-                    }
-                    peticion.resolve();
-                }).catch(function (type, error) {
-                    peticion.reject(error);
-                });
+                        method: 'generate_taxfree_from_order_name',
+                        args: [order.name],
+                    });
 
-                return peticion;
-            }
-
-            push_single_order(order, opts) {
-                // push_and_invoice_order
-                opts = opts || {};
-                var self = this;
-                var push_order_promise = old_prototype_posmodel.push_single_order.call(self, order);
-
-                if (!order.is_to_taxfree()) {
-                    return push_order_promise;
-                }
-
-                var verificaciones = $.Deferred();
-
-                if(!order.get_partner()) {
-                    return verificaciones.reject({code:400, message:'Missing Customer', data:{}});
-                }
-
-                if (order.get_total_tax()==0) {
-                    return verificaciones.reject({code:'9584', message:'La factura no tiene IVA', error_taxfree:true});
-                }
-
-
-                $.when(this.test_tourist()).then(function(resultado) {
-                    if ("code" in resultado && resultado.code == '0000') {
-                        var order_number = order.name;
-                        push_order_promise.then(function() {
-                            self.create_taxfree(order_number).then(function(resultado) {
-                                if ("code" in resultado && resultado.code == '0000') {
-                                    order.set_taxfree_pdf(resultado.check);
-                                    order.set_taxfree_number(resultado.number);
-                                    verificaciones.resolve();
-                                } else {
-                                    order.error_taxfree = 'Ha habido un error inesperado creando tax free. Por favor, comuniquese con su central para la creación manual.';
-                                    if ("code" in resultado) {
-                                        order.error_taxfree += ' Detalles: '+resultado['code'];
-                                        if ("msg" in resultado) {
-                                            order.error_taxfree += '. '+resultado['msg'];
-                                        }
-                                    }
-                                    verificaciones.resolve();
-                                }
-                            }).catch(function(error) {
-                                error.error_taxfree = 'Error desconocido';
-                                verificaciones.reject(error);
-                            });
-
-                        }).catch(function(error) {
-                            error.error_taxfree = 'Error desconocido';
-                            verificaciones.reject(error);
-                        });
+                    if (res && res.code === '0000') {
+                        order.set_taxfree_pdf(res.check);
+                        order.set_taxfree_number(res.number);
                     } else {
-                        verificaciones.reject(resultado);
+                        order.error_taxfree = res.msg || 'Error al generar PDF';
                     }
-
-                }).catch(function(resultado) {
-                    verificaciones.reject(resultado);
-                });
-
-                return push_order_promise;
-            }
-        }
-
-    Registries.Model.extend(PosGlobalState, PosExtension);
-
-    var old_prototype = Order.prototype
-    const PosOrderExtension = (Order) =>
-        class PosOrderExtension extends Order {
-            setup() {
-                super.setup();
-
-                this.to_taxfree = false;
-                this.taxfree_pdf = false;
-                this.taxfree_number = false;
-                this.invoice_number = false;
-                this.error_taxfree = false;
-            }
-
-            initialize(attributes, options) {
-                old_prototype.initialize.call(this,attributes,options);
-                return old_prototype.initialize.call(this,attributes,options);
-            }
-
-            set_to_taxfree(to_taxfree) {
-                this.to_taxfree = to_taxfree;
-            }
-
-            is_to_taxfree() {
-                return this.to_taxfree;
-            }
-
-            set_taxfree_pdf(taxfree_pdf) {
-                this.taxfree_pdf = taxfree_pdf;
-            }
-            get_taxfree_pdf() {
-                return this.taxfree_pdf;
-            }
-            set_taxfree_number(taxfree_number) {
-                this.taxfree_number = taxfree_number;
-            }
-            get_taxfree_number() {
-                return this.taxfree_number;
-            }
-
-            export_for_printing() {
-                var self = this;
-                var receipt = old_prototype.export_for_printing.call(this);
-
-                if (self.is_to_invoice()) {
-                    receipt.invoice_id = {name: self.invoice_number};
+                } catch (e) {
+                    order.error_taxfree = 'Error de comunicación tras crear pedido';
                 }
-
-                return receipt;
             }
-        }
 
-    Registries.Model.extend(Order, PosOrderExtension);
+            return server_ids;
+        }
+    };
+
+    Registries.Model.extend(PosGlobalState, PosTaxFreeGlobal);
+
+    const PosTaxFreeOrder = (Order) => class PosTaxFreeOrder extends Order {
+        setup() {
+            super.setup();
+            this.to_taxfree = this.to_taxfree || false;
+            this.taxfree_pdf = this.taxfree_pdf || false;
+            this.taxfree_number = this.taxfree_number || false;
+            this.error_taxfree = this.error_taxfree || false;
+        }
+        set_to_taxfree(val) { this.to_taxfree = val; }
+        is_to_taxfree() { return this.to_taxfree; }
+        set_taxfree_pdf(val) { this.taxfree_pdf = val; }
+        get_taxfree_pdf() { return this.taxfree_pdf; }
+        set_taxfree_number(val) { this.taxfree_number = val; }
+        get_taxfree_number() { return this.taxfree_number; }
+    };
+
+    Registries.Model.extend(Order, PosTaxFreeOrder);
 });
--- travel_tax_free/static/src/js/pos_invoice_taxfree.js
+++ travel_tax_free/static/src/js/pos_invoice_taxfree.js
@@ -1,13 +1,45 @@
 odoo.define("pos_taxfree.PosInvoiceTaxfree", function(require){
-    'use strict;'
+    'use strict';
 
     const Registries = require('point_of_sale.Registries');
     const PaymentScreen = require('point_of_sale.PaymentScreen');
+    const rpc = require('web.rpc');
+    const { Gui } = require('point_of_sale.Gui');
 
     const PosInvoiceTaxfree = (PaymentScreen) =>
         class PosInvoiceTaxfree extends PaymentScreen {
             setup() {
                 super.setup();
+            }
+
+            async validateOrder(isForceValidate) {
+                const order = this.currentOrder;
+
+                if (order.is_to_taxfree()) {
+                    const partner = order.get_partner();
+                    if (!partner) {
+                        Gui.showPopup('ErrorPopup', { title: 'Error', body: 'Seleccione un cliente' });
+                        return;
+                    }
+
+                    try {
+                        const res = await rpc.query({
+                            model: 'res.partner',
+                            method: 'test_tourist_id',
+                            args: [partner.id],
+                        });
+
+                        if (res && res.code !== '0000') {
+                            Gui.showPopup('ErrorPopup', { title: 'Error Tax Free', body: res.msg });
+                            return;
+                        }
+                    } catch (err) {
+                        console.error("Error en validación RPC:", err);
+                        Gui.showPopup('ErrorPopup', { title: 'Error de Conexión', body: 'No se pudo validar el turista.' });
+                        return;
+                    }
+                }
+                return super.validateOrder(isForceValidate);
             }
 
             click_taxfree() {
@@ -25,7 +57,6 @@ odoo.define("pos_taxfree.PosInvoiceTaxfree", function(require){
             }
 
             toggleIsToInvoice() {
-                // click_invoice
                 super.toggleIsToInvoice();
                 var order = this.env.pos.get_order();
                 if (!order.is_to_invoice() && order.is_to_taxfree()) {
@@ -33,32 +64,10 @@ odoo.define("pos_taxfree.PosInvoiceTaxfree", function(require){
                 }
             }
 
-            async _postPushOrderResolve(order, server_ids){
-                // post_push_order_resolve
-                var self = this;
-
-                return new Promise(function (resolve, reject) {
-                    var promise = this.super(order, server_ids);
-
-                    promise.then(function() {
-                        if (!order.is_to_invoice()) {
-                            resolve();
-                        } else {
-                            self.pos.get_invoice_number(order).then(function() {
-                                resolve();
-                            }).catch(function(error) {
-                                reject(error)
-                            });
-                        }
-
-                    }).catch(function(error) {
-                        reject(error);
-                    });
-                })
+            get isTaxFreeActive() {
+                return this.currentOrder ? this.currentOrder.is_to_taxfree() : false;
             }
-       };
+        };
 
     Registries.Component.extend(PaymentScreen, PosInvoiceTaxfree);
-    return PosInvoiceTaxfree;
-
 });
